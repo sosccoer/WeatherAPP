56cf3d440cf1a127cdffa775fd3dae8c3943f5fe
soscooter <lelya.rumynin@gmail.com>
2023-09-27 20:08:23 +0300

    lottie fix

--- Weather/Weather/SplashViewController.swift
+++ Weather/Weather/SplashViewController.swift
@@ -10,6 +10,7 @@ import Alamofire
 import Lottie
 
 class SplashViewController: UIViewController {
+    
     @IBOutlet weak var lottieAnimation: LottieAnimationView!
     
     private var animation: LottieAnimationView!
@@ -18,15 +19,13 @@ class SplashViewController: UIViewController {
         super.viewDidLoad()
         
         setupAnimation()
-            
-        
         
         makeCurrentWeather()
         
     }
     
     private func setupAnimation () {
-        
+        lottieAnimation.backgroundColor = UIColor(red: 0x38/255.0, green: 0x99/255.0, blue: 0xE0/255.0, alpha: 1.0)
         animation = .init(name: "Weather" )
         animation.loopMode = .playOnce
         animation.contentMode = .scaleAspectFit
@@ -34,17 +33,17 @@ class SplashViewController: UIViewController {
         animation.animationSpeed = 1
         lottieAnimation.addSubview(animation)
         animation.play(){ [weak self] (finished) in
+            
             if finished {
-                let mainViewController = MainViewController() 
+                
+                let mainViewController = MainViewController()
                 mainViewController.modalPresentationStyle = .fullScreen
                 
                 self?.present(mainViewController,animated: true)
+                
             }
             
         }
-        
-        
-        
         
     }
     
