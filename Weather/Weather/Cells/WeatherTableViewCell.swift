eb3902b8d99744e13b929215be1bd3a1e3779c6d
soscooter <lelya.rumynin@gmail.com>
2023-09-29 17:17:10 +0300

    add tableView for MainViewController

---
+++ Weather/Weather/Cells/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  Weather
+//
+//  Created by lelya.rumynin@gmail.com on 29.09.23.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Weather/Weather/ViewControllers/MainViewController.swift
+++ Weather/Weather/ViewControllers/MainViewController.swift
@@ -9,19 +9,64 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     private let apiWeather = ApiWeather()
     
     var lastRespons: RealTimeWeatherRespons?
-    
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         apiWeather.delegate = self
         
+        setupTableView()
+        
     }
     
+    func setupTableView() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        registerTableViewCells()
+        
+    }
+    
+    func registerTableViewCells () {
+        
+        let nibName = UINib(nibName: "WeatherTableViewCell", bundle: Bundle.main)
+        tableView.register(nibName, forCellReuseIdentifier: "WeatherTableViewCell")
+        
+    }
+      
+}
+
+extension MainViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherTableViewCell", for: indexPath) as? WeatherTableViewCell else {return UITableViewCell()}
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.row == 0 {
+            return 80.0 // Высота для первой ячейки
+        } else if indexPath.row == 1 {
+            return 120.0 // Высота для второй ячейки
+        } else {
+            return 60.0 // Высота для остальных ячеек
+        }
+        
+    }
+  
 }
 
 extension MainViewController: WeatherAPIDelegate {
@@ -40,5 +85,9 @@ extension MainViewController: WeatherAPIDelegate {
     }
     
 }
+
+
+
+
 
 
