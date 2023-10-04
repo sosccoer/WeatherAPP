565427dccd5aa3993c3defda0d3c7d5d04d8d3fa
soscooter <lelya.rumynin@gmail.com>
2023-10-04 03:41:15 +0300

    add func choose

--- Weather/Weather/ViewControllers/SettingsViewController.swift
+++ Weather/Weather/ViewControllers/SettingsViewController.swift
@@ -8,25 +8,26 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
+
     let cells: [TypeOfTableViewCell] = [
-    
-        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Velocity Type",nameOfValues: ["km/h","mp/h","m/s"]),
-        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Pressure Type",nameOfValues: ["in","mb","mmHg"]),
-        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Precipitation Type",nameOfValues: ["in","mm"]),
-        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Temperature Type",nameOfValues: ["°С","°F"]),
-        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Space Type",nameOfValues: ["km","mile"])
+        
+        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Velocity Type",nameOfValues: [VelocityType.kph.rawValue,VelocityType.mph.rawValue,VelocityType.mps.rawValue]),
+        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Pressure Type",nameOfValues: [PressureType.inches.rawValue,PressureType.mb.rawValue,PressureType.mmHg.rawValue]),
+        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Precipitation Type",nameOfValues: [PrecipitationType.inches.rawValue,PrecipitationType.mm.rawValue]),
+        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Temperature Type",nameOfValues: [TemperatureType.celsius.rawValue,TemperatureType.fahrenheit.rawValue]),
+        TypeOfTableViewCell(type: .SettingTableViewCell,nameOfSetting: "Space Type",nameOfValues: [SpaceType.km.rawValue,SpaceType.mile.rawValue])
         
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor.clear
         setupTableView()
     }
-
+    
     private func setupTableView() {
         
         tableView.delegate = self
@@ -41,7 +42,14 @@ class SettingsViewController: UIViewController {
         tableView.register(settingNib, forCellReuseIdentifier: "SettingTableViewCell")
     }
     
-
+    private func chose(_ title: String, in selector: UISegmentedControl) {
+        for segment in 0..<selector.numberOfSegments {
+            if selector.titleForSegment(at: segment) == title {
+                selector.selectedSegmentIndex = segment
+            }
+        }
+    }
+    
 }
 
 extension SettingsViewController: UITableViewDelegate,UITableViewDataSource {
@@ -61,12 +69,18 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource {
             
             cell.label.text = cells[index].nameOfSetting
             cell.segmentControl.removeAllSegments()
+            
+            for value in cells[index].nameOfValues.indices{
                 
-                for value in cells[index].nameOfValues.indices{
-                    
-                    cell.segmentControl.insertSegment(withTitle: cells[index].nameOfValues[value], at: value, animated: false)
-                    
-                }
+                cell.segmentControl.insertSegment(withTitle: cells[index].nameOfValues[value], at: value, animated: false)
+                              
+            }
+            
+            chose(Settings.init().velocity.rawValue, in: cell.segmentControl)
+            chose(Settings.init().precipitation.rawValue, in: cell.segmentControl)
+            chose(Settings.init().pressure.rawValue, in: cell.segmentControl)
+            chose(Settings.init().space.rawValue, in: cell.segmentControl)
+            chose(Settings.init().temperature.rawValue, in: cell.segmentControl)
             
             return cell
         }
